975bdd47d84cca42d9b791787be05932c8959feb
anju7566 <chaudharyanju7566@gmail.com>
2022-06-07 10:47:27 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+console.log("hellow");
+console.warn("javascript");
+console.error("anzzu");
+
+
+const aa = 20;
+{
+  let aa = 40;
+  console.log(aa);
+}
+console.log(aa);
+
+
+const bb=10;
+console.log(bb)
+{
+    let bb =2;
+    console.log(bb)
+}
+const cc= 10+10
+console.log(cc)
+{
+    let cc =30-15;
+    console.log(cc);
+}
+const yy=15+15
+{
+    let yy=10+10;
+    console.log(Number(yy))
+}
+console.log(yy)
+
+const aaa=hi;
+{
+    let aaa=hi;
+    console.log(aaa)
+}
+console.log(aaa);
+
+
+var name = "55";
+console.log(typeof name);
+var z = "55";
+console.log(z);
+
+var number = "10";
+console.log(typeof 10);
+
+var age = 30;
+console.log(typeof age);
+
+var date = "a";
+console.log(typeof date);
+
+var x = [1, 2, 3, 4];
+console.log(typeof x);
+
+var y = "15";
+console.log(Number(y));
+
+var a = "hi";
+console.log(String(a));
+
+var b = true;
+console.log(Number(b), String(b));
+
+var d = 0.5;
+console.log(parseInt(d));
+
+var F = 0.1;
+console.log(parseFloat(F));
+
+var e = 20.36;
+console.log(e.toFixed(3));
